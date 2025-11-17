3339a6a9914951f172739255e31b6f61ebfe0d45
FranciscoSolerAguado <franciscojsoleraguado@gmail.com>
2025-11-17 13:44:47 +0100

    Nota.kt

---
+++ app/src/main/java/com/fran/appnotas/model/Nota.kt
@@ -0,0 +1,10 @@
+package com.fran.appnotas.model
+
+data class Nota(
+    val id: Int = 0,
+    var title: String,
+    var content: String,
+    var date: String,
+    var color: String,
+    var category: String
+)

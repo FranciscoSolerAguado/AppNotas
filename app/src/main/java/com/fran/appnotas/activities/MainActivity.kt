064097abfb43a2bc33fad2c84058a32ea091c4f7
FranciscoSolerAguado <franciscojsoleraguado@gmail.com>
2025-11-19 09:23:38 +0100

    Menu superior (idea)

--- app/src/main/java/com/fran/appnotas/activities/MainActivity.kt
+++ app/src/main/java/com/fran/appnotas/activities/MainActivity.kt
@@ -1,21 +1,45 @@
 package com.fran.appnotas.activities
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
 import com.fran.appnotas.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_search -> {
+                // Handle search action
+                true
+            }
+            R.id.action_change_view -> {
+                // Handle change view action
+                true
+            }
+            R.id.action_sort_by -> {
+                // Handle sort by action
+                true
+            }
+            R.id.action_settings -> {
+                // Handle settings action
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
